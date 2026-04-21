2354c3349ecfb125b489cd104c9725255bc7afb8
bryan <bryanmendez21@icloud.com>
2026-04-21 13:54:10 -0400

    started parcing employee information to its correct data types.

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,9 +1,55 @@
 package com.pluralsight;
 
 public class Employee {
+
     // Attributes
     private String name;
     private int employeeId;
     private double hoursWorked;
     private double payRate;
+
+    // Constructor
+    public Employee() {
+        this.name = null;
+        this.employeeId = 0;
+        this.hoursWorked = 0;
+        this.payRate = 0;
+    }
+
+    // Name Setter and Getter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Employee ID Setter and Getter
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    // Hours Worked Setter and Getter
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    // Pay Rate Setter and Getter
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
 }
--- src/main/java/com/pluralsight/PayrollReader.java
+++ src/main/java/com/pluralsight/PayrollReader.java
@@ -1,7 +1,34 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
 public class PayrollReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+        BufferedReader bufReader = new BufferedReader(fileReader);
+        Employee employeeProfile = new Employee(  );
+
+        String dataFromFile;
+
+
+        while ((dataFromFile = bufReader.readLine()) != null){
+
+            String[] splitData = dataFromFile.split(Pattern.quote("|"));
+
+            int employeeID = Integer.parseInt(splitData[0]);
+            String employeeName = splitData[1];
+            double hoursWorked = Double.parseDouble(splitData[2]);
+            double payRate = Double.parseDouble(splitData[3]);
+
+            System.out.println(employeeID);
+
+
+        }
 
     }
+
 }
